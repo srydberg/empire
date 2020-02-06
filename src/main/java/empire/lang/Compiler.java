2b6b6b13fe061e09c9ccd3a0649cd57bbeb4e971
Sebastian Rydberg <sebastian.rydberg@gmail.com>
2020-02-06 21:39:03 +0100

    Add new unit tests for additional cases
    Improve the coverage by adding tests for exception paths:
    
    * Invalid bindings
    * Invalid regex conditions

--- src/main/java/empire/lang/Compiler.java
+++ src/main/java/empire/lang/Compiler.java
@@ -1,121 +1,113 @@
 package empire.lang;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.regex.Pattern;
-
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
-public class Compiler
-{
-	private final InputStream input;
-	private final OutputStream output;
-	private final Map<String,Object> properties;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
 
-	public Compiler( InputStream input, OutputStream output, Map<String, Object> properties ) {
-		this.input = input;
-		this.output = output;
-		this.properties = properties;
-	}
+public class Compiler {
+    private final InputStream input;
+    private final OutputStream output;
+    private final Map<String, Object> properties;
 
-	public void compile() throws IOException {
-		final ANTLRInputStream input;
-		final Writer writer;
+    public Compiler(InputStream input, OutputStream output, Map<String, Object> properties) {
+        this.input = input;
+        this.output = output;
+        this.properties = properties;
+    }
 
-		input = new ANTLRInputStream( new BufferedInputStream( this.input ) );
-		writer = new BufferedWriter( new OutputStreamWriter( this.output ) );
+    public void compile() throws IOException {
+        final ANTLRInputStream input;
+        final Writer writer;
 
-		EmpireLexer lexer = new EmpireLexer( input );
-		CommonTokenStream tokens = new CommonTokenStream( lexer );
+        input = new ANTLRInputStream(new BufferedInputStream(this.input));
+        writer = new BufferedWriter(new OutputStreamWriter(this.output));
 
-		EmpireParser parser = new EmpireParser(tokens);
-		ParseTree tree = parser.prog();
+        EmpireLexer lexer = new EmpireLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
 
-		EmpireBaseListener listener = new EmpireBaseListener() {
-			Map<String, String> bindings = new HashMap<String, String>();
-			public void exitBinding( EmpireParser.BindingContext ctx ) {
-				String property = ctx.Identifier(1).getText();
+        EmpireParser parser = new EmpireParser(tokens);
+        ParseTree tree = parser.prog();
 
-				if( !properties.containsKey( property ) ) {
-					return;
-				}
+        EmpireBaseListener listener = new EmpireBaseListener() {
+            Map<String, String> bindings = new HashMap<>();
+            String regex;
+            Map<String, String> conditions = new HashMap<>();
+            String output;
 
-				Object value = properties.get( property );
-				bindings.put(ctx.Identifier(0).getText(), value.toString());
-			}
+            public void exitBinding(EmpireParser.BindingContext ctx) {
+                String property = ctx.Identifier(1).getText();
 
-			String regex;
-			public void exitLiteral(empire.lang.EmpireParser.LiteralContext ctx) {
-				TerminalNode identifier = ctx.Identifier();
-				TerminalNode regexLiteral = ctx.RegexLiteral();
+                if (!properties.containsKey(property)) {
+                    return;
+                }
 
-				if(identifier != null) {
-					regex = identifier.getText();
-				}
-				else
-				if (regexLiteral != null) {
-					regex = regexLiteral.getText();
-					regex = regex.substring(2, regex.length()-1);
-				}
-			};
+                Object value = properties.get(property);
+                bindings.put(ctx.Identifier(0).getText(), value.toString());
+            }
 
-			Map<String, String> conditions = new HashMap<String,String>();
-			public void exitCondition(empire.lang.EmpireParser.ConditionContext ctx) {
-				conditions.put(ctx.Identifier().getText(), regex);
-				regex=null;
-			};
+            public void exitLiteral(empire.lang.EmpireParser.LiteralContext ctx) {
+                TerminalNode identifier = ctx.Identifier();
+                TerminalNode regexLiteral = ctx.RegexLiteral();
 
-			String output;
-			public void exitOutputBlock(EmpireParser.OutputBlockContext ctx) {
-				String text = ctx.OutputBlock().getText();
-				text = text.substring(1, text.length()-1);
-				output = text.trim();
-			}
+                if (identifier != null) {
+                    regex = identifier.getText();
+                } else if (regexLiteral != null) {
+                    regex = regexLiteral.getText();
+                    regex = regex.substring(2, regex.length() - 1);
+                }
+            }
 
-			public void exitStatement(EmpireParser.StatementContext ctx) {
-				boolean conditionsValid = true;
-				for(String key:conditions.keySet()) {
-					String value = conditions.get(key);
+            public void exitCondition(empire.lang.EmpireParser.ConditionContext ctx) {
+                conditions.put(ctx.Identifier().getText(), regex);
+                regex = null;
+            }
 
-					if(!bindings.containsKey( key )) {
-						conditionsValid = false;
-						break;
-					}
+            public void exitOutputBlock(EmpireParser.OutputBlockContext ctx) {
+                String text = ctx.OutputBlock().getText();
+                text = text.substring(1, text.length() - 1);
+                output = text.trim();
+            }
 
-					String boundValue = bindings.get(key);
-					if(!Pattern.matches(value, boundValue)) {
-						conditionsValid = false;
-						break;
-					}
-				}
+            public void exitStatement(EmpireParser.StatementContext ctx) {
+                boolean conditionsValid = true;
+                for (String key : conditions.keySet()) {
+                    String value = conditions.get(key);
 
-				if(conditionsValid) {
-					try	{
-						writer.write( output );
-						writer.write( '\n' );
-					} catch ( IOException e ) {
-						throw new RuntimeException( e );
-					}
-				}
+                    if (!bindings.containsKey(key)) {
+                        conditionsValid = false;
+                        break;
+                    }
 
-				conditions.clear();
-				properties.clear();
-			}
-		};
+                    String boundValue = bindings.get(key);
+                    if (!Pattern.matches(value, boundValue)) {
+                        conditionsValid = false;
+                        break;
+                    }
+                }
 
-		ParseTreeWalker walker = new ParseTreeWalker();
-		walker.walk( listener, tree );
-		writer.flush();
-	}
+                if (conditionsValid) {
+                    try {
+                        writer.write(output);
+                        writer.write('\n');
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+                conditions.clear();
+                properties.clear();
+            }
+        };
+
+        ParseTreeWalker walker = new ParseTreeWalker();
+        walker.walk(listener, tree);
+        writer.flush();
+    }
 }
--- src/test/java/empire/lang/TestCompiler.java
+++ src/test/java/empire/lang/TestCompiler.java
@@ -1,6 +1,6 @@
 package empire.lang;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -9,83 +9,95 @@ import java.net.URL;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class TestCompiler {
-	@Test
-	public void emptyShouldCompile() throws IOException {
-		String result = compile("");
-		assertEquals("", result);
-	}
+    @Test
+    public void emptyShouldCompile() throws IOException {
+        String result = compile("");
+        assertEquals("", result);
+    }
 
-	@Test
-	public void bareShouldCompile() throws IOException {
-		String result = compile("() {\n}");
-		assertEquals("\n", result);
-	}
+    @Test
+    public void bareShouldCompile() throws IOException {
+        String result = compile("() {a\n}");
+        assertEquals("a\n", result);
+    }
 
-	@Test
-	public void conditionShouldCompile() throws IOException {
-		String result = compile("bind a=a\n(a=b) {\n}","a","b");
-		assertEquals("\n", result);
-	}
+    @Test
+    public void invalidBindingsShouldCompile() throws IOException {
+        String result = compile("bind a=b\n(a=b) {c\n}");
+        assertEquals("", result);
+    }
 
-	@Test
-	public void regexConditionShouldCompile() throws IOException {
-		String result = compile("bind a=a\n(a=~/[b]/) {c\n}","a","b");
-		assertEquals("c\n", result);
-	}
+    @Test
+    public void conditionShouldCompile() throws IOException {
+        String result = compile("bind a=a\n(a=b) {c\n}", "a", "b");
+        assertEquals("c\n", result);
+    }
 
-	@Test
-	public void regexEscapeConditionShouldCompile() throws IOException {
-		String result = compile("bind a=a\n(a=~/\\/b/) {c\n}","a","/b");
-		assertEquals("c\n", result);
-	}
+    @Test
+    public void regexConditionShouldCompile() throws IOException {
+        String result = compile("bind a=a\n(a=~/[b]/) {c\n}", "a", "b");
+        assertEquals("c\n", result);
+    }
 
-	@Test
-	public void shouldCompileFile() throws IOException {
-		String source = readUTF8File( "Test.empire" );
-		String result = compile( source, "b", "x", "d", "y" );
-		assertEquals("property=value\nkey=value\n", result);
-	}
+    @Test
+    public void invalidRegexConditionShouldCompile() throws IOException {
+        String result = compile("bind a=a\n(a=~/[b]/) {c\n}", "a", "x");
+        assertEquals("", result);
+    }
+
+    @Test
+    public void regexEscapeConditionShouldCompile() throws IOException {
+        String result = compile("bind a=a\n(a=~/\\/b/) {c\n}", "a", "/b");
+        assertEquals("c\n", result);
+    }
+
+    @Test
+    public void shouldCompileFile() throws IOException {
+        String source = readUTF8File("Test.empire");
+        String result = compile(source, "b", "x", "d", "y");
+        assertEquals("property=value\nkey=value\n", result);
+    }
 
 
-	/**
-	 * Helper methods
-	 *
-	 */
+    /**
+     * Helper methods
+     */
 
-	private String compile(String source, String... props ) throws IOException {
-		Map<String,Object> properties = new HashMap<String,Object>();
-		for(int i=0; i<props.length;i+=2) {
-			properties.put(props[i],props[i+1]);
-		}
+    private String compile(String source, String... props) throws IOException {
+        Map<String, Object> properties = new HashMap<>();
+        for (int i = 0; i < props.length; i += 2) {
+            properties.put(props[i], props[i + 1]);
+        }
 
-		ByteArrayInputStream input = new ByteArrayInputStream(source.getBytes());
-		ByteArrayOutputStream output = new ByteArrayOutputStream();
+        ByteArrayInputStream input = new ByteArrayInputStream(source.getBytes());
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
 
-		Compiler compiler = new Compiler( input, output, properties);
-		compiler.compile();
+        Compiler compiler = new Compiler(input, output, properties);
+        compiler.compile();
 
-		return new String(output.toByteArray());
-	}
+        return new String(output.toByteArray());
+    }
 
-	private String readUTF8File( String file ) throws IOException {
-		URL resource = this.getClass().getResource( "/" + file );
+    private String readUTF8File(String file) throws IOException {
+        URL resource = this.getClass().getResource("/" + file);
 
-		if( resource == null )
-			throw new IOException("Failed to get '" + file + "'");
+        if (resource == null)
+            throw new IOException("Failed to get '" + file + "'");
 
-		byte[] encoded = Files.readAllBytes( Paths.get( resource.getPath() ) );
+        byte[] encoded = Files.readAllBytes(Paths.get(resource.getPath()));
 
-		Charset utf8 = Charset.forName( "UTF-8" );
-		CharBuffer buffer = utf8.decode( ByteBuffer.wrap( encoded ) );
+        Charset utf8 = StandardCharsets.UTF_8;
+        CharBuffer buffer = utf8.decode(ByteBuffer.wrap(encoded));
 
-		return buffer.toString();
-	}
+        return buffer.toString();
+    }
 }
